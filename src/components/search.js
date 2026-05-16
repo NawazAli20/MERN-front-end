9b0dfc1112bd04197f7710e9bc150ca4312f87c5
Nawaz.Ali20 <gga@g.clemson.edu>
2026-05-16 14:18:22 -0500

    lowercase name condition is added

--- src/components/search.js
+++ src/components/search.js
@@ -1,39 +1,39 @@
-import { Link, useSearchParams } from 'react-router-dom';
-import StudentsDb from '../sources/studentDb';
+import {Link, useSearchParams} from 'react-router-dom'
+import StudentsDB from '../sources/studentDb';
 
 function Search(){
     const [searchParams, setSearchParams] = useSearchParams({"name":""});
-    const searchText = searchParams.get("name")?.toLowerCase(); 
-    let students = []; 
-    if(searchParams.get("name")){
-        students = StudentsDb.filter(student=>student.name.includes(searchText))
-    } 
-    return(    
-        <>
-            <h3>Search a student by name</h3>
-            <form>
-                <input type='text'
-                placeholder='John Doe'
-                value={searchParams.get("name")||""}
-                onChange={(e)=>setSearchParams({"name":e.target.value})}
-                />
-            </form>
-            {
-                students.length>0 && 
-                <ul>
-                {
-                students.map(student =>
-                    <li key={student.id}>{student.id} &nbsp; &nbsp;
-                        <Link to={`/students/${student.id.toString()}`}>{student.name}</Link>
-                    </li>
-                
-                )
-                }
-                </ul>
-            }
-        </>
-    )
 
+    const searchText = searchParams.get("name")?.trim().toLowerCase(); 
+    let students = [];
+
+    if(searchParams.get("name")){
+        students =  StudentsDB.filter(student=>student.name.toLowerCase().includes(searchText));
+    }
+
+    return(
+        <>
+        <h2>Search for a student by name:</h2>
+        <input type='text'
+        placeholder='John Doe'
+        value={searchParams.get("name")||""}
+        onChange={(e)=>setSearchParams({"name":e.target.value})}
+        />
+        {
+            students.length>0 &&
+            <ul>
+                {
+                    students.map(student=>
+                        <li key={student.id}>{student.id}: 
+                        <Link to={`/students/${student.id}`}>{student.name}</Link>
+                        </li>
+                    )
+                }
+            </ul>
+        }
+
+        </>
+    );
 }
 
 export default Search;
